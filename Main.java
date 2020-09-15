cb29c96992c99d11b6821c607e0b07460967bd13
khrid0328 <khrid0328@wrdsb.ca>
2020-09-15 13:47:26 +0000

    A2Q1: PRACTICE QUESTION

--- Main.java
+++ Main.java
@@ -1,15 +1,33 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * This program will determine if a number the user inputs is even or odd
+ * @author Dafna Khripun
  */
 public class Main {
 
   /**
    * The method that is executed when you hit the run button.
+   * 
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user for one integer
+    System.out.println("Please enter an integer:");
+    // declare and initialize a variable for integer 1
+    int integer = input.nextInt();
+
+    // declare and calculate the remainder when divided by 2
+    int remainder = integer % 2;
+
+    // telling the user weather the integer is even or odd
+    if (remainder == 0) {
+      System.out.println(integer + " is an even number");
+    } else {
+      System.out.println(integer + " is an odd number");
+    }
   }
 }
